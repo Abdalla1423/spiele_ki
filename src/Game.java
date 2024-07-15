f51d2cb6f64ffa24504a724e55fbeb76a395cc00
Abdalla Eltayeb <abdalla.eltayeb@sap.com>
2024-07-15 14:50:18 +0200

    cleanup and generate new jar

--- src/Game.java
+++ src/Game.java
@@ -1,12 +1,6 @@
-import org.junit.jupiter.api.Assertions;
 
-import java.lang.reflect.Array;
 import java.util.*;
-import java.util.concurrent .TimeUnit;
 
-import static java.lang.Math.abs;
-import static java.lang.Math.max;
-import static java.lang.System.currentTimeMillis;
 
 public class Game {
     //gibt Player R zurück, wenn rot gewonnen hat; Player B zurück, wenn blau gewonnen hat; Player EMPTY zurück, wenn noch
@@ -35,93 +29,20 @@ public class Game {
     }
 
     public static long numOfSearchedZustand = 0;
-    static HashMap<String, int[]> alphabetacutoffmove = new LinkedHashMap<>();
-
-    static  int maxdepth; // zu currentdepth umbennenen
     static HashMap<BoardDepthKey, MoveEvaluation> transpositionTable = new HashMap<>();
 
     public static MoveEvaluation iterativeDeepening(Board startBoard, int maxDepth, boolean useAlphaBeta) {
-        /*MoveEvaluation bestMoveEvaluation = new MoveEvaluation(0, "");
-        bestMoveEvaluation = minimaxAlphaBeta(startBoard, 5, Integer.MIN_VALUE, Integer.MAX_VALUE, useAlphaBeta, "");
-        System.out.println("Anzhal Zustände: " + numOfSearchedZustand);
-        return bestMoveEvaluation;
-         */
-
-        //maxdepth soll vom timemanagement ausgewählt werden
-
-        /*int initialWindowSize = 50;
-        int windowSizeIncrement = 70;
-        int initialEvaluation = 0;
-
-         */
-
 
         numOfSearchedZustand = 0;
         MoveEvaluation bestMoveEvaluation = new MoveEvaluation(0, new int[20]);
         for (int depth = 1; depth <= maxDepth; depth++) {
-            // maxdepth = depth;
             bestMoveEvaluation = minimaxAlphaBeta(startBoard, depth, Integer.MIN_VALUE, Integer.MAX_VALUE, useAlphaBeta, new int[20], new ArrayList<int[]>());
-
-            /*int alphaAspirationWindow = initialEvaluation - initialWindowSize;
-            int betaAspirationWindow = initialEvaluation + initialWindowSize;
-
-            // Try the initial window size
-            bestMoveEvaluation = minimaxAlphaBeta(startBoard, depth, alphaAspirationWindow, betaAspirationWindow, useAlphaBeta, new int[20], new ArrayList<int[]>());
-            initialEvaluation = bestMoveEvaluation.evaluation;
-
-            //System.out.println("Alpha: " + alphaAspirationWindow + ", Beta: " + betaAspirationWindow + ", Evaluation: " + bestMoveEvaluation.evaluation + ", Move: " + Move.moveToString(bestMoveEvaluation.move));
-
-            //initialEvaluation = bestMoveEvaluation.evaluation;
-
-            // Check if the result is outside the window
-            if (bestMoveEvaluation.evaluation <= alphaAspirationWindow || bestMoveEvaluation.evaluation >= betaAspirationWindow) {
-                // Increase the window size and try again
-                alphaAspirationWindow = initialEvaluation - (initialWindowSize + windowSizeIncrement);
-                betaAspirationWindow = initialEvaluation + (initialWindowSize + windowSizeIncrement);
-
-                bestMoveEvaluation = minimaxAlphaBeta(startBoard, depth, alphaAspirationWindow, betaAspirationWindow, useAlphaBeta, new int[20], new ArrayList<int[]>());
-                initialEvaluation = bestMoveEvaluation.evaluation;
-                //System.out.println("Alpha: " + alphaAspirationWindow + ", Beta: " + betaAspirationWindow + ", Evaluation: " + bestMoveEvaluation.evaluation + ", Move: " + Move.moveToString(bestMoveEvaluation.move));
-
-
-                // If still outside the window, fall back to normal alpha-beta
-                if (bestMoveEvaluation.evaluation <= alphaAspirationWindow || bestMoveEvaluation.evaluation >= betaAspirationWindow) {
-                    bestMoveEvaluation = minimaxAlphaBeta(startBoard, depth, Integer.MIN_VALUE, Integer.MAX_VALUE, useAlphaBeta, new int[20], new ArrayList<int[]>());
-                    initialEvaluation = bestMoveEvaluation.evaluation;
-                    //System.out.println("Alpha: " + alphaAspirationWindow + ", Beta: " + betaAspirationWindow + ", Evaluation: " + bestMoveEvaluation.evaluation + ", Move: " + Move.moveToString(bestMoveEvaluation.move));
-                }
-            }
-
-             */
-            //System.out.println("Alpha: " + alphaAspirationWindow + ", Beta: " + betaAspirationWindow + ", Evaluation: " + bestMoveEvaluation.evaluation + ", Move: " + Move.moveToString(bestMoveEvaluation.move));
-
-
-            // Update der anfänglichen Schätzung für die nächste Iterationstiefe
-
-
-            //Umdrehen des Arrays damit Züge in richtiger Reihenfolge
-            /*int[] bestmovepfadtrash = bestMoveEvaluation.move;
-            int z = 0;
-            for(int i = bestmovepfadtrash.length-2; i > 0; i--) {
-                if(bestmovepfadtrash[i] != 0 ){
-                    bestmovepfad[z] = bestmovepfadtrash[i-1];
-                    bestmovepfad[z+1] = bestmovepfadtrash[i];
-                    i--;
-                    z+=2;
-                }
-            }
-
-            bestMoveEvaluation.move[0] = bestmovepfad[0];
-            bestMoveEvaluation.move[1] = bestmovepfad[1];
-
-             */
 
         }
 
         return bestMoveEvaluation;
     }
 
-    static int[] bestmovepfad = new int[20];
 
     public static MoveEvaluation minimaxAlphaBeta(Board board, int depth, int alpha, int beta, boolean useAlphaBeta, int[] lastMove, ArrayList<int[]> currMoves) {
         int moveFactor = currMoves.size() * 20;
@@ -143,44 +64,7 @@ public class Game {
 
         MoveEvaluation bestMove = new MoveEvaluation(board.blauIstDran ? Integer.MIN_VALUE : Integer.MAX_VALUE, new int[20]);
 
-        /*if(alphabetacutoffmove.containsKey(Arrays.toString(lastMove))){
-            int [] cutoffmove = alphabetacutoffmove.get(Arrays.toString(lastMove));
-            List allPossibleMove = allPossibleMoves.stream().filter(i -> i[0] != cutoffmove[0] && i[1] != cutoffmove[1]).toList();
-            allPossibleMoves = new ArrayList<>(allPossibleMove);
-            allPossibleMoves.add(0, cutoffmove);
-        }
-
-        int frompo = bestmovepfad[(maxdepth-depth)*2];
-        int topo = bestmovepfad[(maxdepth-depth)*2+1];
-        int z2 = 0;
-
-        if(bestmovepfad[(maxdepth-depth)*2] != 0){
-            allPossibleMoves.add(0, new int[]{bestmovepfad[(maxdepth-depth)*2], bestmovepfad[(maxdepth-depth)*2+1]});
-            bestmovepfad[(maxdepth-depth)*2] = 0;
-        }
-
-         */
-
         for (int[] move : allPossibleMoves) {
-            /*if (moves[0] == frompo && moves[1] == topo & z2!= 0) {
-                //Zug war der beste zug und wurde an erster Stelle gesetzt
-            }else{
-                z2++;
-                int[] move = new int[20];
-                move[0] = moves[0];
-                move[1] = moves[1];
-                int z = 0;
-                int z1 = 2;
-                if(lastMove[0] != 0){
-                    while(lastMove[z] != 0){
-                        move[z1] = lastMove[z];
-                        move[z1+1] = lastMove[z+1];
-                        z += 2;
-                        z1 += 2;
-                    }
-                }
-
-             */
                 Board nextBoard = new Board(board.blauzweiteebene, board.rotzweiteebene, board.blauersteebene, board.rotersteebene);
                 nextBoard.updateBoard(move[0], move[1]);
                 nextBoard.blauIstDran = !board.blauIstDran;
@@ -213,30 +97,24 @@ public class Game {
                 }
 
                 if (useAlphaBeta && beta <= alpha) {
-                    //alphabetacutoffmove.put(Arrays.toString(Arrays.copyOfRange(move, 2, 22)), new int[]{move[0], move[1]});
                     break; // Alpha-beta cut-off
                 }
             }
-        // }
 
         // Transpositiontable
-        //System.out.println("Statements:" + depth + " " + alpha + " " + beta);
         transpositionTable.put(new BoardDepthKey(board, depth), bestMove);
 
         return bestMove;
     }
 
-    public static String getMove(String b, int time) {
+    public static String getMove(String b) {
         Board board = new Board(b);
-        int maxDepth = timeManagment(board, time);
-        MoveEvaluation res = Game.iterativeDeepening(board, maxDepth, true);
+        MoveEvaluation res = Game.iterativeDeepening(board, 6, true);
         return Move.moveToString(res.move);
     }
 
     public static void main(String[] args) {
-        // Board startBoard = new Board("b01b0b0b0b0/1b0b01b01b01/3b01b02/2b05/8/2r0r01rr2/1r04r01/r0r0r0r0r0r0 r");
-        // MoveEvaluation bestMoveEvaluation = iterativeDeepening(startBoard, 5, useAlphaBeta);
-        String move = getMove("b01b0b0b0b0/1b0b01b01b01/3b01b02/2b05/8/2r0r01rr2/1r04r01/r0r0r0r0r0r0 r", 100000);
+        String move = getMove("b01b0b0b0b0/1b0b01b01b01/3b01b02/2b05/8/2r0r01rr2/1r04r01/r0r0r0r0r0r0 r");
         System.out.println("Best move: " + move );
     }
 }
